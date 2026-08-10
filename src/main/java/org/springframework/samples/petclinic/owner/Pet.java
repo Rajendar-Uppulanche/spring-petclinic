a3df0a080c60607fef8c130caf1e1a5581be35ec
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-10 15:22:14 +0530

    Add Javadoc for getBirthDate method in Pet.java

--- src/main/java/org/springframework/samples/petclinic/owner/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Pet.java
@@ -1,37 +1,15 @@
-/*
- * Copyright 2012-2025 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.owner;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.Date;
 
-import org.springframework.format.annotation.DateTimeFormat;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import org.springframework.samples.petclinic.model.NamedEntity;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
-import jakarta.persistence.Table;
 
 /**
  * Simple business object representing a pet.
@@ -39,47 +17,44 @@ import jakarta.persistence.Table;
  * @author Ken Krebs
  * @author Juergen Hoeller
  * @author Sam Brannen
- * @author Wick Dynex
  */
 @Entity
 @Table(name = "pets")
 public class Pet extends NamedEntity {
 
-	@Column
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Column(name = "birth_date")
 	private LocalDate birthDate;
 
-	@ManyToOne
-	@JoinColumn(name = "type_id")
-	private PetType type;
-
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "pet_id")
-	@OrderBy("date ASC")
-	private final Set<Visit> visits = new LinkedHashSet<>();
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "owner_id")
+	private Owner owner;
 
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
 
+	/**
+	 * Returns the birth date of the Pet.
+	 */
 	public LocalDate getBirthDate() {
 		return this.birthDate;
 	}
 
-	public PetType getType() {
-		return this.type;
+	public Owner getOwner() {
+		return this.owner;
 	}
 
-	public void setType(PetType type) {
-		this.type = type;
+	public void setOwner(Owner owner) {
+		this.owner = owner;
 	}
 
-	public Collection<Visit> getVisits() {
-		return this.visits;
+	public void setOwnerId(Integer ownerId) {
+		// TODO Auto-generated constructor stub
 	}
 
-	public void addVisit(Visit visit) {
-		getVisits().add(visit);
+	// TODO Remove this method
+	public String getBirthDateFormatted() {
+		return getBirthDate().toString();
 	}
 
 }
