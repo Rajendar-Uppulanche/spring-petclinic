0d7a7e20fd134d93da0f3b37a837d90aa9314cf3
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-11 20:06:44 +0530

    Implement spec.md v1 changes

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+# Configuration settings
+
+# Database settings
+SQLALCHEMY_DATABASE_URI = "sqlite:///petclinic.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Payment service settings
+PAYMENT_RETRY_LIMIT = 3
+
+# Other settings can be added here
