a5e613c1ff562a991747c88c01cc7cacb7d1b954
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-13 10:07:11 +0530

    Implement visit_controller_spec.md v1 changes

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -35,10 +35,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 class VetController {
 
-	private final VetRepository vetRepository;
+	private final VetService vetService; // Changed from VetRepository
 
-	public VetController(VetRepository vetRepository) {
-		this.vetRepository = vetRepository;
+	public VetController(VetService vetService) { // Changed constructor parameter
+		this.vetService = vetService;
 	}
 
 	@GetMapping("/vets.html")
@@ -59,7 +59,7 @@ class VetController {
 	private Page<Vet> findPaginated(int page) {
 		int pageSize = 5;
 		Pageable pageable = PageRequest.of(page - 1, pageSize);
-		return vetRepository.findAll(pageable);
+		return vetService.findVetsPaginated(pageable); // Changed to vetService
 	}
 
 	@GetMapping({ "/vets" })
@@ -67,7 +67,7 @@ class VetController {
 		// Here we are returning an object of type 'Vets' rather than a collection of Vet
 		// objects so it is simpler for JSon/Object mapping
 		Vets vets = new Vets();
-		vets.getVetList().addAll(this.vetRepository.findAll());
+		vets.getVetList().addAll(this.vetService.findAllVets()); // Changed to vetService
 		return vets;
 	}
 
