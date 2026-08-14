4a80aea698d7822f2e503ef716cc69d57664c605
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-14 14:45:29 +0530

    Implement spec 3.md v1 changes

---
+++ src/main/java/org/springframework/samples/petclinic/owner/PetDetailsDto.java
@@ -0,0 +1,54 @@
+package org.springframework.samples.petclinic.owner;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class PetDetailsDto {
+
+    private Integer id;
+    private String name;
+    private LocalDate birthDate;
+    private PetType type;
+    private int visitCount; // New field
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public int getVisitCount() {
+        return visitCount;
+    }
+
+    public void setVisitCount(int visitCount) {
+        this.visitCount = visitCount;
+    }
+}
