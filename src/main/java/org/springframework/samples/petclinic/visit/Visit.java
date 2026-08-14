0227b95c1d0887d1fa0ba0f63a20e9f1e3f1f496
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-14 16:32:15 +0530

    Implement spec 3.md v1 changes

---
+++ src/main/java/org/springframework/samples/petclinic/visit/Visit.java
@@ -0,0 +1,59 @@
+package org.springframework.samples.petclinic.visit;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.owner.Pet;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity {
+
+	@Column(name = "visit_date")
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate date;
+
+	@NotEmpty
+	@Column(name = "description")
+	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
+	private Pet pet;
+
+	public Visit() {
+		this.date = LocalDate.now();
+	}
+
+	public LocalDate getDate() {
+		return this.date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Pet getPet() {
+		return this.pet;
+	}
+
+	public void setPet(Pet pet) {
+		this.pet = pet;
+	}
+
+}
