a18d4d57ecc79be7285755afc9eb6786fe9cea4c
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 18:09:33 +0530

    Implement spec 3.md v1 changes

--- src/main/java/org/springframework/samples/petclinic/owner/Visit.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Visit.java
@@ -1,68 +1,86 @@
-/*
- * Copyright 2012-2025 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.owner;
-
-import java.time.LocalDate;
-
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.samples.petclinic.model.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.veterinarian.Veterinarian; // Import the new Veterinarian entity
+
+import java.time.LocalDate;
 
 /**
  * Simple JavaBean domain object representing a visit.
- *
- * @author Ken Krebs
- * @author Dave Syer
  */
 @Entity
 @Table(name = "visits")
-public class Visit extends BaseEntity {
+public class Visit { // Not extending BaseEntity as it's not provided, will add ID directly
 
-	@Column(name = "visit_date")
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private LocalDate date;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
 
-	@NotBlank
-	private String description;
+    @Column(name = "visit_date")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate date;
 
-	/**
-	 * Creates a new instance of Visit for tomorrow
-	 */
-	public Visit() {
-		this.date = LocalDate.now().plusDays(1);
-	}
+    @NotEmpty
+    @Column(name = "description")
+    private String description;
 
-	public LocalDate getDate() {
-		return this.date;
-	}
+    @Column(name = "pet_id") // Assuming pet_id is a direct column for simplicity
+    private Integer petId;
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+    @ManyToOne
+    @JoinColumn(name = "veterinarian_id") // New foreign key column
+    private Veterinarian veterinarian; // New field for the veterinarian
 
-	public String getDescription() {
-		return this.description;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPetId() {
+        return this.petId;
+    }
+
+    public void setPetId(Integer petId) {
+        this.petId = petId;
+    }
+
+    public Veterinarian getVeterinarian() {
+        return veterinarian;
+    }
+
+    public void setVeterinarian(Veterinarian veterinarian) {
+        this.veterinarian = veterinarian;
+    }
 }
