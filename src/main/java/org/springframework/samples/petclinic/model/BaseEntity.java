5aa4fc6dbe7612f969da88a4a3186759b9ca96d0
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 18:11:04 +0530

    Implement spec 3.md v1 changes

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2012-2025 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.model;
-
-import java.io.Serializable;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,18 +6,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
 /**
- * Simple JavaBean domain object with an id property. Used as a base class for objects
- * needing this property.
- *
- * @author Ken Krebs
- * @author Juergen Hoeller
+ * Base class for JPA entities. Provides a common `id` field and `isNew()` method.
  */
 @MappedSuperclass
-public class BaseEntity implements Serializable {
+public class BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	protected Integer id;
 
 	public Integer getId() {
 		return id;
