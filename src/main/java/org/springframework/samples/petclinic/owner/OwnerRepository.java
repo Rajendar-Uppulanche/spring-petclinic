3e69dd6cc27fdf09005cf3a6ba1948c99fcc2faa
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 10:06:43 +0530

    Implement spec 3.md v1 changes

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
@@ -22,7 +22,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Repository class for <code>Owner</code> domain objects. All method names are compliant
+ * Repository class for `Owner` domain objects. All method names are compliant
  * with Spring Data naming conventions so this interface can easily be extended for Spring
  * Data. See:
  * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.query-creation
@@ -34,7 +34,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Wick Dynex
  */
 public interface OwnerRepository extends JpaRepository<Owner, Integer> {
-
 	/**
 	 * Retrieve {@link Owner}s from the data store by last name, returning all owners
 	 * whose last name <i>starts</i> with the given name.
@@ -43,6 +42,16 @@ public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 	 * found)
 	 */
 	Page<Owner> findByLastNameStartingWith(String lastName, Pageable pageable);
+
+	/**
+	 * Retrieve {@link Owner}s from the data store by last name, returning all owners
+	 * whose last name <i>contains</i> the given name, case-insensitively, ordered by
+	 * last name then first name.
+	 * @param lastName Value to search for
+	 * @param pageable Pagination and sorting information
+	 * @return a Page of matching {@link Owner}s (or an empty Page if none found)
+	 */
+	Page<Owner> findByLastNameContainingIgnoreCaseOrderByLastNameAscFirstNameAsc(String lastName, Pageable pageable);
 
 	/**
 	 * Retrieve an {@link Owner} from the data store by id.
