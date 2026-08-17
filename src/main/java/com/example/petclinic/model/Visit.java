639b18b2d515f0f16ded961aca6b1078cfc97b48
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 17:54:44 +0530

    Implement spec 3.md v1 changes

---
+++ src/main/java/com/example/petclinic/model/Visit.java
@@ -0,0 +1,83 @@
+package com.example.petclinic.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity {
+    @Column(name = "visit_date")
+    private LocalDate date;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    @ManyToOne
+    @JoinColumn(name = "veterinarian_id")
+    private Veterinarian veterinarian;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Veterinarian getVeterinarian() {
+        return veterinarian;
+    }
+
+    public void setVeterinarian(Veterinarian veterinarian) {
+        this.veterinarian = veterinarian;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Visit visit = (Visit) o;
+        return Objects.equals(date, visit.date) &&
+               Objects.equals(description, visit.description) &&
+               Objects.equals(pet, visit.pet) &&
+               Objects.equals(veterinarian, visit.veterinarian);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), date, description, pet, veterinarian);
+    }
+
+    @Override
+    public String toString() {
+        return "Visit{" +
+               "id=" + getId() +
+               ", date=" + date +
+               ", description='" + description + '\'' +
+               ", pet=" + (pet != null ? pet.getName() : "null") +
+               ", veterinarian=" + (veterinarian != null ? veterinarian.getName() : "null") +
+               '}';
+    }
+}
