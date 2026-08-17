9b0c78ebf75d49669bd30f50842ef93b337b5d9f
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 17:48:45 +0530

    Implement spec 3.md v1 changes

---
+++ src/main/java/org/springframework/samples/petclinic/vet/VeterinarianRepository.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.vet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VeterinarianRepository extends JpaRepository<Veterinarian, Integer> {
+    List<Veterinarian> findByLastName(String lastName);
+}
