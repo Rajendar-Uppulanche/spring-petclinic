9e6491ba028eb3cf4ac8224bf77fa8fa5b6745ab
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 17:46:15 +0530

    Implement visit_controller_spec.md v1 changes

---
+++ src/main/java/org/springframework/samples/petclinic/visit/VisitFilterCriteria.java
@@ -0,0 +1,46 @@
+package org.springframework.samples.petclinic.visit;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class VisitFilterCriteria {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+    private String petName;
+    private String ownerLastName;
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public String getOwnerLastName() {
+        return ownerLastName;
+    }
+
+    public void setOwnerLastName(String ownerLastName) {
+        this.ownerLastName = ownerLastName;
+    }
+}
