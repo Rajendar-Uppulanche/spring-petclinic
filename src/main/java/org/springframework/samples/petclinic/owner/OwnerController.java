aef36b87a1ef4ac5b64ebeac01661257818b1a7d
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 10:05:52 +0530

    Implement visit_controller_spec.md v1 changes

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -133,7 +133,7 @@ class OwnerController {
 	private Page<Owner> findPaginatedForOwnersLastName(int page, String lastname) {
 		int pageSize = 5;
 		Pageable pageable = PageRequest.of(page - 1, pageSize);
-		return owners.findByLastNameStartingWith(lastname, pageable);
+		return owners.findByLastNameContainingIgnoreCase(lastname, pageable);
 	}
 
 	@GetMapping("/owners/{ownerId}/edit")
