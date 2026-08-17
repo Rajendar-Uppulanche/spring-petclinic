36d6287600be21c5e3b4e6e0dc63398f420d8d15
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 13:25:51 +0530

    Implement SP-36 v1 changes

--- src/main/java/org/springframework/samples/petclinic/owner/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Pet.java
@@ -1,27 +1,4 @@
-/*
- * Copyright 2012-2025 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.owner;
-
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.Set;
-
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.samples.petclinic.model.NamedEntity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -32,38 +9,46 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.NamedEntity;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
- * Simple business object representing a pet.
+ * Simple JavaBean domain object representing a pet.
  *
  * @author Ken Krebs
  * @author Juergen Hoeller
  * @author Sam Brannen
- * @author Wick Dynex
  */
 @Entity
 @Table(name = "pets")
 public class Pet extends NamedEntity {
 
-	@Column
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Column(name = "birth_date")
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	private LocalDate birthDate;
 
 	@ManyToOne
 	@JoinColumn(name = "type_id")
 	private PetType type;
 
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "pet_id")
-	@OrderBy("date ASC")
-	private final Set<Visit> visits = new LinkedHashSet<>();
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
+	private Owner owner;
 
-	public void setBirthDate(LocalDate birthDate) {
-		this.birthDate = birthDate;
-	}
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet", fetch = FetchType.LAZY)
+	@OrderBy("visit_date DESC")
+	private Set<Visit> visits = new LinkedHashSet<>();
 
 	public LocalDate getBirthDate() {
 		return this.birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
 	}
 
 	public PetType getType() {
@@ -74,12 +59,36 @@ public class Pet extends NamedEntity {
 		this.type = type;
 	}
 
-	public Collection<Visit> getVisits() {
+	public Owner getOwner() {
+		return this.owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+
+	protected Set<Visit> getVisitsInternal() {
+		if (this.visits == null) {
+			this.visits = new LinkedHashSet<>();
+		}
 		return this.visits;
 	}
 
+	protected void setVisitsInternal(Set<Visit> visits) {
+		this.visits = visits;
+	}
+
+	public Set<Visit> getVisits() {
+		return getVisitsInternal();
+	}
+
 	public void addVisit(Visit visit) {
-		getVisits().add(visit);
+		getVisitsInternal().add(visit);
+		visit.setPet(this);
+	}
+
+	public int getVisitsCount() {
+		return getVisitsInternal().size();
 	}
 
 }
