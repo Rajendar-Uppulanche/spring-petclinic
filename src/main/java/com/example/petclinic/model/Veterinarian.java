d8f594597dfe719fde37c7ef3c7f6b836b7c470f
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 17:53:07 +0530

    Implement spec 3.md v1 changes

---
+++ src/main/java/com/example/petclinic/model/Veterinarian.java
@@ -0,0 +1,63 @@
+package com.example.petclinic.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "veterinarians")
+public class Veterinarian extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "contact_information")
+    private String contactInformation;
+
+    public Veterinarian() {
+    }
+
+    public Veterinarian(String name, String contactInformation) {
+        this.name = name;
+        this.contactInformation = contactInformation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Veterinarian that = (Veterinarian) o;
+        return Objects.equals(name, that.name) &&
+               Objects.equals(contactInformation, that.contactInformation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name, contactInformation);
+    }
+
+    @Override
+    public String toString() {
+        return "Veterinarian{" +
+               "id=" + getId() +
+               ", name='" + name + '\'' +
+               ", contactInformation='" + contactInformation + '\'' +
+               '}';
+    }
+}
