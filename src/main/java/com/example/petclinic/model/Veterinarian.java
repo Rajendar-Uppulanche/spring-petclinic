47bf4429503ac774b6f4bcbfc595e1d3370c98b1
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-17 18:00:54 +0530

    Implement spec 3.md v1 changes

---
+++ src/main/java/com/example/petclinic/model/Veterinarian.java
@@ -0,0 +1,49 @@
+package com.example.petclinic.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "veterinarians")
+public class Veterinarian {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotEmpty
+    private String name;
+
+    @Column(name = "contact_information")
+    private String contactInformation;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+}
