15ece5ef5ea463fd56962b19eed65a43326a81f2
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-19 14:05:26 +0530

    Implement visit_controller_spec.md v1 changes

---
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerServiceTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.owner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * Unit tests for {@link OwnerService}.
+ *
+ * @author Wick Dynex
+ */
+@ExtendWith(MockitoExtension.class)
+class OwnerServiceTest {
+
+	@Mock
+	private OwnerRepository ownerRepository;
+
+	@InjectMocks
+	private OwnerService ownerService;
+
+	private Owner owner1;
+	private Owner owner2;
+	private Owner owner3;
+
+	@BeforeEach
+	void setUp() {
+		owner1 = new Owner();
+		owner1.setId(1);
+		owner1.setFirstName(
