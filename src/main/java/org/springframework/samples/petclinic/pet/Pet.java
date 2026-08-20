6dde60f4ec2469bc34ab3bb3de98f097b5835c9c
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-20 17:45:08 +0530

    Implement visit_controller_spec.md v1 changes

---
+++ src/main/java/org/springframework/samples/petclinic/pet/Pet.java
@@ -0,0 +1,49 @@
+package org.springframework.samples.petclinic.pet;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.NamedEntity;
+import org.springframework.samples.petclinic.owner.Owner;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pets")
+public class Pet extends NamedEntity {
+
+    @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType type;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+}
