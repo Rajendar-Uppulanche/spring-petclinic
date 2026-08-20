be4822e9d3e4274ebd7c71fd7ac3b6bba39ea65e
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-20 15:53:35 +0530

    Implement visit_controller_spec.md v1 changes

---
+++ src/main/java/com/example/model/VaccineType.java
@@ -0,0 +1,26 @@
+package com.example.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "vaccine_types")
+public class VaccineType extends BaseEntity {
+
+    @Column(name = "name")
+    @NotEmpty
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
