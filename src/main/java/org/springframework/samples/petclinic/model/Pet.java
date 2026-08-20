58ec2b0cd2211bca7a86952e3d31e5e3d956e682
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-20 09:56:31 +0530

    Implement SP-40 v1 changes

---
+++ src/main/java/org/springframework/samples/petclinic/model/Pet.java
@@ -0,0 +1,69 @@
+package org.springframework.samples.petclinic.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * Simple JavaBean domain object representing a pet.
+ *
+ * @author Ken Krebs
+ * @author Juergen Hoeller
+ * @author Sam Brannen
+ */
+@Entity
+@Table(name = "pets")
+public class Pet extends NamedEntity {
+
+	@Column(name = "birth_date")
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@NotNull
+	@PastOrPresent(message = "{pet.birthDate.future}")
+	private LocalDate birthDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "type_id")
+	@NotNull
+	private PetType type;
+
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
+	private Owner owner;
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public LocalDate getBirthDate() {
+		return this.birthDate;
+	}
+
+	public PetType getType() {
+		return this.type;
+	}
+
+	public void setType(PetType type) {
+		this.type = type;
+	}
+
+	public Owner getOwner() {
+		return this.owner;
+	}
+
+	protected void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+
+	public boolean isNew() {
+		return this.id == null;
+	}
+
+}
