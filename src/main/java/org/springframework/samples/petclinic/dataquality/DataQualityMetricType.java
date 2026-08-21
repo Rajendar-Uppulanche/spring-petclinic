45911cc798f9cd1102a8f1f2ac83efbf6d85801d
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-21 12:03:54 +0530

    Implement visit_controller_spec.md v1 changes

---
+++ src/main/java/org/springframework/samples/petclinic/dataquality/DataQualityMetricType.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.dataquality;
+
+/**
+ * Enum representing different types of data quality metrics.
+ */
+public enum DataQualityMetricType {
+
+	COMPLETENESS,
+	VALIDITY
+
+}
