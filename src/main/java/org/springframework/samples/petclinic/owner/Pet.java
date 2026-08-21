c828f36bc6b5a3445fff9e5f839906366917251c
Rajendar-Uppulanche <uppulanche_r@xoriant.com>
2026-08-21 12:03:59 +0530

    Implement visit_controller_spec.md v1 changes

--- src/main/java/org/springframework/samples/petclinic/owner/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Pet.java
@@ -42,44 +42,4 @@ import jakarta.persistence.Table;
  * @author Wick Dynex
  */
 @Entity
-@Table(name = "pets")
-public class Pet extends NamedEntity {
-
-	@Column
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private LocalDate birthDate;
-
-	@ManyToOne
-	@JoinColumn(name = "type_id")
-	private PetType type;
-
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "pet_id")
-	@OrderBy("date ASC")
-	private final Set<Visit> visits = new LinkedHashSet<>();
-
-	public void setBirthDate(LocalDate birthDate) {
-		this.birthDate = birthDate;
-	}
-
-	public LocalDate getBirthDate() {
-		return this.birthDate;
-	}
-
-	public PetType getType() {
-		return this.type;
-	}
-
-	public void setType(PetType type) {
-		this.type = type;
-	}
-
-	public Collection<Visit> getVisits() {
-		return this.visits;
-	}
-
-	public void addVisit(Visit visit) {
-		getVisits().add(visit);
-	}
-
-}
+@Table(name = 
